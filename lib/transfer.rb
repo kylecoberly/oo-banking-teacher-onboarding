397d105b1a15c6a8e2ec4d390cb281a7eaf4c57d
essential-gemfile-5901 <kyle.coberly@gmail.com>
2019-03-04 05:32:58 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,7 +1,8 @@
 class Transfer
-  attr_reader :sender, :receiver
+  attr_reader :sender, :receiver, :pending
   def initialize sender, receiver, amount
     @sender = sender
     @receiver = receiver
+    @status = "pending"
   end
 end
