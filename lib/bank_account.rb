e6004d9a57531ef278067a32d09bc702fea85741
essential-gemfile-5901 <kyle.coberly@gmail.com>
2019-03-04 05:17:20 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -12,11 +12,11 @@ class BankAccount
   end
 
   def display_balance
-    "Your balance is $#{balance}."
+    "Your balance is $#{@balance}."
   end
 
   def valid?
-    @status && balance
+    @status && @balance
   end
 
   def close_account
